c3120d4b281092c6de5049bfff7f247f50f552d4
Mauricio Arrechea <mauricioarrechea@pop-os.localdomain>
2021-12-30 13:13:06 +0100

    added question template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,9 +23,15 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show a question "%s" !',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+        $answers = [
+            'Trabajar',
+            'Perder el tiempo',
+            'Nada',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
